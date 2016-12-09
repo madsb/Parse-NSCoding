b1967dfec181ec282f6c64ae1ac8554e8bc46cdc
drdaz <drdaz7@gmail.com>
2016-12-09 14:34:25 +0100

    Removes properties that don't make any sense

--- PFObject+NSCoding.m
+++ PFObject+NSCoding.m
@@ -18,9 +18,6 @@
 #define kPFObjectObjectId @"___PFObjectId"
 #define kPFObjectAllKeys @"___PFObjectAllKeys"
 #define kPFObjectClassName @"___PFObjectClassName"
-#define kPFObjectCreatedAtKey @"___PFObjectCreatedAt"
-#define kPFObjectUpdatedAtKey @"___PFObjectUpdatedAt"
-#define kPFObjectIsDataAvailableKey @"hasBeenFetched"
 
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
@@ -28,11 +25,6 @@
 	[encoder encodeObject:[self objectId] forKey:kPFObjectObjectId];
 	[encoder encodeObject:[self parseClassName] forKey:kPFObjectClassName];
 	[encoder encodeObject:[self allKeys] forKey:kPFObjectAllKeys];
-	[encoder encodeBool:[self isDataAvailable] forKey:kPFObjectIsDataAvailableKey];
-	
-	//Serialize Parse timestamps
-	[encoder encodeObject:self.createdAt forKey:kPFObjectCreatedAtKey];
-	[encoder encodeObject:self.updatedAt forKey:kPFObjectUpdatedAtKey];
 	
 	//Serialize all non-nil Parse properties
 	//[self allKeys] returns only the @dynamic properties that are not nil
@@ -40,10 +32,6 @@
 		id value = self[key];
 		[encoder encodeObject:value forKey:key];
 	}
-	
-	//Serialize all non-Parse properties
-	NSDictionary* nonParseProperties = [self nonDynamicProperties];
-	[self encodeProperties:nonParseProperties withCoder:encoder];
 }
 
 - (id)initWithCoder:(NSCoder*)aDecoder
@@ -52,7 +40,6 @@
     NSString* objectId = [aDecoder decodeObjectForKey:kPFObjectObjectId];
 	NSString* parseClassName = [aDecoder decodeObjectForKey:kPFObjectClassName];
 	NSArray* allKeys = [aDecoder decodeObjectForKey:kPFObjectAllKeys];
-	BOOL isDataAvailable = [aDecoder decodeBoolForKey:kPFObjectIsDataAvailableKey];
 	
 	if ([self isMemberOfClass:[PFObject class]]) {
 		//If this is a PFObject, recreate the object using the Parse class name and objectId
@@ -64,11 +51,6 @@
 	}
 	
 	if (self) {
-		
-		//Deserialize Parse timestamps
-//        [self setValue:[aDecoder decodeObjectForKey:kPFObjectCreatedAtKey] forKey:@"createdAt"];
-//        [self setValue:[aDecoder decodeObjectForKey:kPFObjectUpdatedAtKey] forKey:@"updatedAt"];
-		
 		//Deserialize all non-nil Parse properties
 		for (NSString* key in allKeys) {
             id obj = [aDecoder decodeObjectForKey:key];
@@ -86,17 +68,7 @@
 				self[key] = [NSNull null];
 			}
 		}
-		
-		//Deserialize all non-Parse properties
-		NSDictionary* nonParseProperties = [self nonDynamicProperties];
-		//[self decodeProperties:nonParseProperties withCoder:aDecoder];
     }
-	
-	//Mark PFObject as not dirty
-	[self->operationSetQueue removeAllObjects];
-	
-	//Mark PFObject with same hasBeenFetched value as before encoding
-	[self setValue:@(isDataAvailable) forKey:kPFObjectIsDataAvailableKey];
 	
     NSLog(@"Initialized object %@", self);
     
